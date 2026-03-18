337e809d46ed9e99901faae055860cea1b08ba92
SuperSweatyYeti <supersweatyeti@gmail.com>
2026-03-18 00:31:32 -0700

    update 2026-03-18 00:31:32

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -319,3 +319,8 @@ function prompt {
     }
 }
 
+# zoxide config
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+}
+
--- readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -310,3 +310,8 @@ function prompt {
     }
 }
 
+# zoxide config
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+}
+
